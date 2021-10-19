483194da395de68366cfd63d4f2499708f269190
ZobayerSakib <zobayersakib44@gmail.com>
2021-10-19 15:47:16 +0600

    error page disign

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './ErrorPage.css'
+import { Link } from 'react-router-dom';
 const ErrorPage = () => {
     return (
-        <div>
+        <div className='errorPageDesign'>
             <h1>404</h1>
+            <p>Opps! your searching result is not exist. Please Go Back </p>
+            <Link to='/home'>Go Back</Link>
         </div>
     );
 };
